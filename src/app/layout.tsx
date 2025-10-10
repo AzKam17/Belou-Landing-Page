10877d895b1f8a895faa35efbf55caef3f79994f
AmadouAzizKAMAGATE <aziz.kamagate@djamo.io>
2025-10-10 06:20:39 +0000

    added google analytics tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,9 +41,20 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <style suppressHydrationWarning>{splashScreenStyles}</style>
-        
+          <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5T758N4B');</script>
       </head>
       <body className={`antialiased`}>
+      <noscript>
+          <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-5T758N4B"
+              height="0" width="0"
+              style="display:none;visibility:hidden"
+          ></iframe>
+      </noscript>
         {/* <div id="splash-screen">
           <Image
             alt="Logo"
