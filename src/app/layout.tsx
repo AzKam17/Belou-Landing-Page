7d30b9cb1dfafa07102b14ceb8b4552bbb42e8f0
AmadouAzizKAMAGATE <aziz.kamagate@djamo.io>
2025-10-10 06:30:28 +0000

    added google analytics tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { GoogleAnalytics } from '@next/third-parties/google'
 import '@/assets/scss/style.scss'
 import AppProviders from '../components/wrappers/AppProviders'
 
@@ -41,20 +42,8 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <style suppressHydrationWarning>{splashScreenStyles}</style>
-          <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-5T758N4B');</script>
       </head>
       <body className={`antialiased`}>
-      <noscript>
-          <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-5T758N4B"
-              height="0" width="0"
-              style="display:none;visibility:hidden"
-          ></iframe>
-      </noscript>
         {/* <div id="splash-screen">
           <Image
             alt="Logo"
@@ -69,6 +58,7 @@ export default function RootLayout({
           <AppProviders>{children}</AppProviders>
         </div>
       </body>
+      <GoogleAnalytics gaId="GTM-5T758N4B" />
     </html>
   )
 }
